6bf87b21e511fea145114698d6757b3931dde1ef
Ethan Burkley <ethan.burkley@colorado.edu>
2018-04-24 16:39:26 -0600

    Hello

--- flow.m
+++ flow.m
@@ -7,15 +7,18 @@ x1step = 1; x2step = 1;
 [x1, x2] = meshgrid(x1min:1:x2max, x2min:1:x2max);
 
 %set parameter values 
-a = 1; 
+a = 0.831;
+b = 0.0162;
+c = 0.2824;
+d = 0.0211;
 
 % Define the system of equations 
 %for this example we consider the equation 
 %           dx1/dt = a*x2
 %           dx2/dt = -x1
 
-dx1 = a*x2;  
-dx2 = -x1; 
+dx1 = -a*x1 + b*x1*x2;  
+dx2 = c*x2 - d*x1*x2; 
 
 %normalize vectors (to help plotting)
 dx1 = dx1./sqrt(dx1.^2 + dx2.^2); 
--- project3.m
+++ project3.m
@@ -1,1 +1,5 @@
-%%Hello
+%% 
+%APPM 2360 Project 3 
+%Ethan Burkley(Section 251), Davis Landry(Section 213), Max Morgan(Section244)
+
+%%
