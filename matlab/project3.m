1f4e4e71b396be9a87dc284f4cf600b356e6e5ab
Ethan Burkley <ethan.burkley@colorado.edu>
2018-04-26 17:52:12 -0600

    t

--- matlab/project3.m
+++ matlab/project3.m
@@ -1,23 +1,23 @@
 %% 
 %APPM 2360 Project 3 
 %Ethan Burkley(Section 251), Davis Landry(Section 213), Max Morgan(Section244)
-
+close all; clear all;
 %% ODE eq and plotting question 2
 [t, x] = ode45(@ODEmat, [0, 50], [10, 40]);%solves using ode45
 %plots the different data out
 figure 
 plot(t, x(:,1))
 hold on
-xlabel("time (t)")
-ylabel("x(t)")
+xlabel("time $(t)$",'Interpreter','latex')
+ylabel("$x(t)$",'Interpreter','latex')
 title("Function behavior over time, x1(t) and x2(t)")
 plot(t, x(:,2))
-legend("x1", "x2")
+legend('x_1', 'x_2')
 hold off
 figure
 hold on
-xlabel("x1")
-ylabel("x2")
+xlabel('$x_1$','Interpreter','latex')
+ylabel('$x_2$','Interpreter','latex')
 title("Parametric curve of solutions for x1 and x2 (0, 50)")
 plot(x(:,1),x(:,2))
 hold off
@@ -36,8 +36,8 @@ legend("x1", "x2")
 hold off
 figure
 hold on
-xlabel("x1")
-ylabel("x2")
+xlabel('$x_1$','Interpreter','latex')
+ylabel('$x_2$','Interpreter','latex')
 title("Parametric curve of solutions for x1 and x2 (0, 50), k = 0.001")
 plot(x(:,1),x(:,2))
 hold off
@@ -55,8 +55,8 @@ legend("x1", "x2")
 hold off
 figure
 hold on
-xlabel("x1")
-ylabel("x2")
+xlabel('$x_1$','Interpreter','latex')
+ylabel('$x_2$','Interpreter','latex')
 title("Parametric curve of solutions for x1 and x2 (0, 50), k=0.02")
 plot(x(:,1),x(:,2))
 hold off
