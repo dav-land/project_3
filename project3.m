0f9cf23a7da0a5230191a44a6db1267c5483f463
Max Morgan <maxmogo@gmail.com>
2018-04-26 17:37:45 -0600

    hello

--- project3.m
+++ project3.m
@@ -60,3 +60,22 @@ ylabel("x2")
 title("Parametric curve of solutions for x1 and x2 (0, 50), k=0.02")
 plot(x(:,1),x(:,2))
 hold off
+%% ode for question 4.1.1
+[t, x] = ode45(@ODEmat411, [0, 200], [10, 40]);%solves using ode45
+%plots the different data out
+figure 
+plot(t, x(:,1))
+hold on
+xlabel("time (t)")
+ylabel("x(t)")
+title("Function behavior over time,k=0.02, x1(t) and x2(t)")
+plot(t, x(:,2))
+legend("x1", "x2")
+hold off
+figure
+hold on
+xlabel("x1")
+ylabel("x2")
+title("Parametric curve of solutions for x1 and x2 (0, 50), k=0.02")
+plot(x(:,1),x(:,2))
+hold off
