a8fe9fc6568a3f00bd9f161dbea2cc10495903bd
Davis Landry <davislandry@Macintosh.local>
2018-04-26 16:57:14 -0600

    yeah

---
+++ ODEmat314.m
@@ -0,0 +1,13 @@
+%function to put ODE into matrix and then use ode45 to solve
+
+function dx = ODEmat314(t, x)
+a = .831; %initial Conditions
+b = .0162;
+c = .2824;
+d = .0211;
+k = .02;
+dx = zeros(2, 1);% create matrix to keep track of solution
+dx(1) = -a*x(1) + b*x(1).*x(2);%the first derivative of x1
+dx(2) = c*x(2)*(1-k*x(1)) - d*x(1).*x(2);%the first derivative of x2
+
+end
